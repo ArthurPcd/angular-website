c65ef5c2c2bbc1c7f826734d7ae52356d62980b1
Joan THOMAS <thomas.joan.pl@gmail.com>
2023-11-22 05:26:44 +0100

    start front login logout navigation'

---
+++ front/src/screens/login/login.component.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  formData = {email: '', password: ''};
+
+  constructor(private authService: AuthService, private router: Router) {
+  }
+
+  handleLogin() {
+    this.authService.tryLogin(this.formData)
+      .subscribe(
+        () => {
+          this.router.navigate(['/dashboard']);
+        },
+        error => {
+          console.error('Error logging in:', error);
+          alert('Une erreur s\'est produite lors de la connexion. Veuillez réessayer.');
+        },
+      );
+  }
+}
