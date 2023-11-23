636106cd2dc3cdee9c2f1021cd1a762dc668dbf8
Joan THOMAS <thomas.joan.pl@gmail.com>
2023-11-23 07:15:42 +0100

    add token to cookies

--- front/app/js/app.js
+++ front/app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('front', ['ngRoute']);
+var app = angular.module('front', ['ngRoute', 'ngCookies']);
 app.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
--- front/app/js/controllers/authCtrl.js
+++ front/app/js/controllers/authCtrl.js
@@ -1,34 +1,35 @@
-app.controller('authCtrl', function($scope, $location, $http) {
-    $scope.login = {};
-    $scope.register = {};
-    $scope.isAdmin = false;
+app.controller('authCtrl', function ($scope, $location, $http, $cookies) {
+  $scope.login = {};
+  $scope.register = {};
+  $scope.isAdmin = false;
 
-    $scope.submitLogin = function() {
-        $http.post('http://localhost:8080/api/auth/login', {email: $scope.login.email, password: $scope.login.password}, {
-          transformResponse: [function (data) {
-            return data;
-          }]
-        })
-          .then(function(response) {
-              const token = response.data;
-              $location.path('/admin');
-              alert('Connexion réussie ! Bienvenue sur le panel d\'administration !');
-          })
-          .catch(function(error) {
-              console.error('Error logging in:', error);
-              alert('Échec de la connexion. Vérifiez vos identifiants.');
-          });
-    };
+  $scope.submitLogin = function () {
+    $http.post('http://localhost:8080/api/auth/login', {email: $scope.login.email, password: $scope.login.password}, {
+      transformResponse: [function (data) {
+        return data;
+      }]
+    })
+      .then(function (response) {
+        const token = response.data;
+        $cookies.put('token', token);
+        $location.path('/admin');
+        alert('Connexion réussie ! Bienvenue sur le panel d\'administration !');
+      })
+      .catch(function (error) {
+        console.error('Error logging in:', error);
+        alert('Échec de la connexion. Vérifiez vos identifiants.');
+      });
+  };
 
-    $scope.capitalizeFirstLetter = function(string) {
-        return string.charAt(0).toUpperCase() + string.slice(1);
-    };
+  $scope.capitalizeFirstLetter = function (string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
-    $scope.submitRegister = function() {
-        var firstNameCapitalized = $scope.capitalizeFirstLetter($scope.register.firstName);
-        var lastNameCapitalized = $scope.capitalizeFirstLetter($scope.register.lastName);
+  $scope.submitRegister = function () {
+    var firstNameCapitalized = $scope.capitalizeFirstLetter($scope.register.firstName);
+    var lastNameCapitalized = $scope.capitalizeFirstLetter($scope.register.lastName);
 
-        console.log('Inscription:', $scope.register);
-        alert('Inscription réussie pour ' + firstNameCapitalized + ' ' + lastNameCapitalized);
-    };
+    console.log('Inscription:', $scope.register);
+    alert('Inscription réussie pour ' + firstNameCapitalized + ' ' + lastNameCapitalized);
+  };
 });
